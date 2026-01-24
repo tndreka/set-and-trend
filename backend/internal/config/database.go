1b977e636f767c70ee7d30fbeb34e8b82906b82a
tndreka <tndreka@student.42heilbronn.de>
2026-01-24 12:42:14 +0000

    fixed syntax error in fmt.Errorf call

--- backend/internal/config/database.go
+++ backend/internal/config/database.go
@@ -11,11 +11,8 @@ import (
 )
 
 func NewDatabase(ctx context.Context, cfg *Config) (*db.Queries, *pgxpool.Pool, error) {
-	//dsn := fmt. Sprintf(
-	//	"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-	//	cfg. DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBSSLMode,
-	//)
 
+	//creat Url PostgreSQL
 	dsn := fmt.Sprintf(
 	"postgres://%s:%s@%s:%s/%s?sslmode=%s",
 	cfg.DBUser,
@@ -25,26 +22,27 @@ func NewDatabase(ctx context.Context, cfg *Config) (*db.Queries, *pgxpool.Pool, 
 	cfg.DBName,
 	cfg.DBSSLMode,
 	)
-
+	//convert credentials in tu URL and return  config obj
 	poolConfig, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		return nil, nil, fmt.Errorf("pgxpool.ParseConfig: %w", err)
 	}
-
+	//define rules for connection pool
 	poolConfig.MaxConns = 100
 	poolConfig.MinConns = 10
-	poolConfig. MaxConnLifetime = time.Hour
+	poolConfig.MaxConnLifetime = time.Hour
 	poolConfig.MaxConnIdleTime = 30 * time.Minute
-	poolConfig. HealthCheckPeriod = time.Minute
-	poolConfig. ConnConfig.ConnectTimeout = 5 * time.Second
+	poolConfig.HealthCheckPeriod = time.Minute
+	poolConfig.ConnConfig.ConnectTimeout = 5 * time.Second
 	
+	//create pool
 	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
 	if err != nil {
 		return nil, nil, fmt.Errorf("pgxpool.NewWithConfig: %w", err)
 	}
 
 	if err := pool.Ping(ctx); err != nil {
-		return nil, nil, fmt. Errorf("ping: %w", err)
+		return nil, nil, fmt.Errorf("ping: %w", err)
 	}
 
 	return db.New(pool), pool, nil
